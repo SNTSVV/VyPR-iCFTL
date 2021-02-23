5b259b6fac73820327a9625d9953ebca696379a6
Joshua Heneage Dawes <joshua.dawes@uni.lu>
2021-02-23 14:18:23 +0100

    Add new test file.

---
+++ tests/test-data/programs/test2.py
@@ -0,0 +1,10 @@
+from mod import f1, f2
+
+def func3():
+    string = "hello"
+    if string == "hello":
+        f1()
+    else:
+        f2()
+        for (n, m) in enumerate(l):
+            print(n, m)
