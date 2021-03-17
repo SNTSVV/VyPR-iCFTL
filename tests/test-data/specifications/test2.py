95ce2f55a9d8fc65ac4e522f99a32e10d1bd6c1a
Joshua Heneage Dawes <joshua.dawes@uni.lu>
2021-03-17 13:57:39 +0100

    Add new specification with two quantifiers to testing files.

---
+++ tests/test-data/specifications/test2.py
@@ -0,0 +1,11 @@
+specification = Specification()\
+    .forall(q = changes('a').during('test4.func1'))\
+    .forall(t = future(calls('g').during('test4.func2')))\
+    .check(
+        lambda q, t : (
+            all_are_true(
+                q('a') < 20,
+                t.duration() < 1
+            )
+        )
+    )
