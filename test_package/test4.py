ad07eb0138f4518fd2eb1f7c4dbe03e6421dc051
Joshua Heneage Dawes <joshua.dawes@uni.lu>
2021-03-17 13:59:10 +0100

    Add files from test_package.

---
+++ test_package/test4.py
@@ -0,0 +1,32 @@
+from VyPR.Monitoring.online import OnlineMonitor
+import logging
+import test_package.vypr_config
+
+def g():
+    print("g called!")
+
+def func1():
+    for i in range(2):
+        a = 10*(i+1)
+        b = 20
+        if b > a:
+            #a = 20
+            func2()
+        else:
+            func2()
+        g()
+
+def func2():
+    print("calling g from func2")
+    g()
+
+if __name__ == "__main__":
+    # configure logging (inspired by stackoverflow for now)
+    FORMAT = '%(asctime)-15s %(message)s'
+    logging.basicConfig(format=FORMAT)
+    # start VyPR monitoring
+    test_package.vypr_config.online_monitor = OnlineMonitor("tests/test-data/specifications/test2.py")
+    # call test function
+    func1()
+    # end VyPR monitoring
+    test_package.vypr_config.online_monitor.end_monitoring()
---
+++ test_package/vypr_config.py
@@ -0,0 +1,6 @@
+"""
+Module to hold VyPR monitoring instance shared across all monitored modules.
+"""
+
+# initialise online monitor to be accessed across modules
+online_monitor = None
