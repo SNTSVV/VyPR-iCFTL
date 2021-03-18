d5382ca119a82928a32f28478c8ad61100125cb7
Joshua Heneage Dawes <joshua.dawes@uni.lu>
2021-03-18 13:13:14 +0100

    Improve logging.

--- VyPR/Monitoring/formula_trees.py
+++ VyPR/Monitoring/formula_trees.py
@@ -1,6 +1,7 @@
 """
 Module that holds all logic regarding binding/formula tree pairs derived while monitoring for an iCFTL specification.
 """
+import logging
 
 from VyPR.Specifications.constraints import is_normal_atom, is_mixed_atom, get_base_variable, Conjunction, Disjunction, Negation
 
@@ -25,13 +26,20 @@ class FormulaTree():
         Store the dictionary of measurements, assuming the form
             {atom index : {subatom index : measurement}}
         """
+        logging.info("Instantiating new formula tree")
         self._timestamps = timestamps
         self._formula_tree = constraint.instantiate()
         self._atoms = constraint.get_atomic_constraints()
         self._variables = variables
         self._measurement_dictionary = measurement_dictionary
+        logging.info("  self._timestamps = %s" % str(self._timestamps))
+        logging.info("  self._formula_tree = %s" % str(self._formula_tree))
+        logging.info("  self._atoms = %s" % str(self._atoms))
+        logging.info("  self._variables = %s" % str(self._variables))
+        logging.info("  self._measurement_dictionary = %s" % str(self._measurement_dictionary))
 
         # run the formula tree update with respect to the measurement dictionary already given
+        logging.info("Updating the formula tree with respect to measurement_dictionary")
         for atom_index in self._measurement_dictionary:
             for subatom_index in self._measurement_dictionary[atom_index]:
                 measurement = self._measurement_dictionary[atom_index][subatom_index]
@@ -83,15 +91,19 @@ class FormulaTree():
         """
         Given a measurement, atom and subatom indices, update the formula tree
         """
+        logging.info("Updating formula tree with measurement = %s with atom_index = %i, subatom_index = %i" % (measurement, atom_index, subatom_index))
         # add the measurement to self._measurement_dictionary
         if atom_index in self._measurement_dictionary:
             if subatom_index not in self._measurement_dictionary[atom_index]:
                 self._measurement_dictionary[atom_index][subatom_index] = measurement
         else:
             self._measurement_dictionary[atom_index] = {subatom_index: measurement}
+        logging.info("Stored measurement in self._measurement_dictionary")
         # recurse on the formula tree
         # assign the result in case there is a truth value
+        logging.info("Recursing on tree to update")
         self._formula_tree = self._recurse_on_tree(self._formula_tree, measurement, atom_index, subatom_index)
+        logging.info("Update finished - result self._formula_tree = %s" % self._formula_tree)
         return self._formula_tree
     
     def _recurse_on_tree(self, current_obj, measurement, atom_index: int, subatom_index: int):
@@ -105,17 +117,23 @@ class FormulaTree():
         we recurse and then check to see whether a truth value can be declared
         for that part of the formula tree.
         """
+        logging.info("Processing current_obj = %s in formula tree traversal" % str(current_obj))
         if is_normal_atom(current_obj) or is_mixed_atom(current_obj):
+            logging.info("Recursive base case - found an atom")
             # base case
             # check to see whether current_obj matches atom_index
+            logging.info("Checking to see if %s matches index %i" % (str(current_obj), atom_index))
             if self._atoms.index(current_obj) == atom_index:
                 # return the answer given by the atom under the measurement given
                 # the answer can be true, false or inconclusive (for mixed atoms)
+                logging.info("current_obj = %s matches - updating with measurement = %s" % (str(current_obj), str(measurement)))
                 return current_obj.check(measurement, subatom_index)
             else:
                 # this isn't the atom we need, so just return it
+                logging.info("No match - continuing traversal")
                 return current_obj
         else:
+            logging.info("Recursive case - found disjunction, conjunction or negation")
             # recursive cases
             # we either have a disjunction, conjunction or negation
 
@@ -124,12 +142,16 @@ class FormulaTree():
             if type(current_obj) is Disjunction:
                 # iterate through the operands, checking whether any are evaluated to True
                 disjuncts = current_obj.get_disjuncts()
+                logging.info("Found disjunction - recursing on disjuncts %s" % disjuncts)
                 for (index, disjunct) in enumerate(disjuncts):
+                    logging.info("Processing disjunct = %s" % str(disjunct))
                     # replace the disjunct with a new value (this may just be the old value if
                     # nothing could be changed given the measurement)
                     disjuncts[index] = self._recurse_on_tree(disjunct, measurement, atom_index, subatom_index)
+                    logging.info("New value for disjunct is %s" % disjunct)
                     # explicitly check for True
                     if disjuncts[index] == True:
+                        logging.info("Since new value is True, and we have a disjunction, replacing disjunction with True")
                         # return True to replace current_obj with True in its parent formula
                         return True
 
@@ -139,33 +161,46 @@ class FormulaTree():
                 # iterate through the operands, checking whether any are evaluated to False
                 # (or whether all are True)
                 conjuncts = current_obj.get_conjuncts()
+                logging.info("Found conjunction - recursing on conjuncts %s" % conjuncts)
                 # count number of True occurrences so we can check for all conjuncts being true
+                logging.info("Setting count of all true conjuncts to 0")
                 number_of_trues = 0
                 for (index, _) in enumerate(conjuncts):
+                    logging.info("Processing conjunct = %s" % str(conjuncts[index]))
                     # replace the conjunct with a new value (this may just be the old value if
                     # nothing could be changed given the measurement)
                     conjuncts[index] = self._recurse_on_tree(conjuncts[index], measurement, atom_index, subatom_index)
+                    logging.info("New value for conjunct is %s" % str(conjuncts[index]))
                     # explicitly check for False
                     if conjuncts[index] == False:
+                        logging.info("conjuncts[index] = False in conjunction, so replacing conjunction with False")
                         # return False to replace current_obj with False in its parent formula
                         return False
                     elif conjuncts[index] == True:
+                        logging.info("conjuncts[index] = True in conjunction, so incrementing the number of trues found")
                         # increase the number of Trues
                         number_of_trues += 1
+                        logging.info("Number of trues/number of conjuncts = %i/%i" % (number_of_trues, len(conjuncts)))
                 # check for all conjuncts being True
                 if number_of_trues == len(conjuncts):
+                    logging.info("Number of trues (%i) = number of conjuncts (%i), so replacing conjunction with True" % (number_of_trues, len(conjuncts)))
                     return True
 
             # in the negation case, we see if the operand gives a truth value
             if type(current_obj) is Negation:
+                logging.info("Found negation - recursing on operand")
                 # recurse on the negation operand, returning True or False if the operand gives a truth value
                 current_obj.operand = self._recurse_on_tree(current_obj.operand, measurement, atom_index, subatom_index)
+                logging.info("New value of negation operand is %s" % str(current_obj.operand))
                 # check truth value
                 if current_obj.operand == True:
+                    logging.info("current_obj.operand = True, so negation becomes False")
                     # negation can be evaluated to False
                     return False
                 elif current_obj.operand == False:
+                    logging.info("current_obj.operand = False, so negation becomes True")
                     # negation can be evaluted to True
                     return True
-            
+
+            logging.info("Returning current_obj = %s to previous level of formula tree" % str(current_obj))
             return current_obj
--- VyPR/Monitoring/online.py
+++ VyPR/Monitoring/online.py
@@ -13,38 +13,50 @@ def monitoring_process_function(online_monitor_object, specification_file):
     """
     Consume measurements from online_monitor_object.queue.
     """
-    # configure logging (inspired by stackoverflow for now)
-    FORMAT = '%(asctime)-15s %(message)s'
+    # configure logging
+    FORMAT = '[%(asctime)-15s] [%(funcName)30s] %(message)s'
     logging.basicConfig(format=FORMAT, filename=f"logs/monitoring/{datetime.datetime.now()}", filemode="a", level=logging.DEBUG)
 
     logging.info("Starting VyPR monitoring process.")
     # read in the specification
     specification = prepare_specification(specification_file)
+    logging.info("Reading in specification from file %s" % specification_file)
     # initialise the stop signal to False
     stop_signal_received = False
     # initialise map from map indices to lists of formula trees
+    logging.info("Initialising empty dictionary map_index_to_formula_trees")
     map_index_to_formula_trees = {}
     # get the list of variables from the specification
+    logging.info("Getting list of variables from specification")
     variables = specification.get_variables()
+    logging.info("Sequence of variables in specification is %s" % str(variables))
     # loop until the end signal is received
+    logging.info("Beginning monitoring loop - loop while stop_signal_received is False")
     while not stop_signal_received:
         # get the event from the front of the queue
         new_measurement = online_monitor_object.get_new_measurement()
         # check type of measurement
         if new_measurement["type"] == "stop_signal":
             # set stop signal
-            logging.info("Received stop signal - setting stop flag")
+            logging.info("Received stop signal instrument")
             stop_signal_received = True
+            logging.info("stop_signal_received = %s" % stop_signal_received)
         elif new_measurement["type"] == "trigger":
+            logging.info("Received trigger instrument")
+            logging.info("  map_index = %s" % new_measurement["map_index"])
+            logging.info("  variable = %s" % new_measurement["variable"])
             # get timestamp for this trigger
+            logging.info("Getting timestamp to be used in new formula tree")
             trigger_timestamp = datetime.datetime.now()
             # get the index of the variable
+            logging.info("Getting index of variable in sequence of variables")
             variable_index = variables.index(new_measurement["variable"])
             # get the map index
             map_index = new_measurement["map_index"]
-            logging.info(f"Received trigger instrument with variable_index = {variable_index}, map_index = {map_index}")
+            logging.info("variable_index = %i" % variable_index)
             # check for an existing list of formula trees with this index
             if not map_index_to_formula_trees.get(map_index):
+                logging.info("Initialising empty set of formula trees in map_index_to_formula_trees for map_index = %i" % map_index)
                 map_index_to_formula_trees[map_index] = []
             # if variable_index == 0, we generate a new binding/formula tree pair
             # and add map_index_to_formula_trees under the key map_index
@@ -52,47 +64,60 @@ def monitoring_process_function(online_monitor_object, specification_file):
             # under the key map_index and extend the ones whose bindings are of length variable_index
 
             # get the constraint held by the specification
+            logging.info("Getting constraint part of specification ready for formula tree instantiation")
             constraint = specification.get_constraint()
+            logging.info("Got constraint = %s from specification" % str(constraint))
 
             # check the variable index
             if variable_index == 0:
-                logging.info(f"Instantiating new formula tree with timestamp {trigger_timestamp}")
+                logging.info("Instantiating new formula tree with timestamp %s" % str(trigger_timestamp))
                 # construct a sequence consisting of a single timestamp
+                logging.info("Constructing list containing a single timestamp")
                 current_timestamp_sequence = [trigger_timestamp]
                 # generate new binding/formula tree pair
                 new_formula_tree = FormulaTree(current_timestamp_sequence, constraint, variables)
+                logging.info("New formula tree %s instantiated" % str(new_formula_tree))
                 # add to the appropriate list of formula trees
+                logging.info("Adding formula tree to map_index_to_formula_trees")
                 map_index_to_formula_trees[map_index].append(new_formula_tree)
             else:
-                print("processing", variable_index)
-                logging.info(f"Instantiating formula tree using existing ones")
+                logging.info("Instantiating formula tree using existing ones in map_index_to_formula_trees")
                 # get existing formula trees
                 formula_trees = map_index_to_formula_trees[map_index]
                 # iterate through the formula trees
                 for formula_tree in formula_trees:
                     # decide whether we need to extend the binding attached to the formula tree
-                    logging.info(f"Inspecting formula tree {formula_tree}")
+                    logging.info("Inspecting formula tree %s" % formula_tree)
                     # get the timestamp sequence from the formula tree
                     timestamps = formula_tree.get_timestamps()
                     # check whether the length of the timestamp sequence is equal to variable_index
                     if len(timestamps) == variable_index:
+                        logging.info("Using state in formula tree %s to instantiate a new one" % formula_tree)
                         # generate an extended timestamp sequence
                         extended_timestamp_sequence = [t for t in timestamps] + [trigger_timestamp]
+                        logging.info("extended_timestamp_sequence = %s" % extended_timestamp_sequence)
                         # get the assignment of atoms/expressions to measurements from formula_tree
                         measurements = formula_tree.get_measurements_for_variable_index(variable_index)
+                        logging.info("measurements = %s" % measurements)
                         # instantiate new formula tree with the extended timestamp sequence, and the measurements
                         # associated with variables from the old formula tree
                         extended_formula_tree = FormulaTree(extended_timestamp_sequence, constraint, variables, measurements)
+                        logging.info("extended_formula_tree = %s" % str(extended_formula_tree))
                         # store the new formula tree
                         map_index_to_formula_trees[map_index].append(extended_formula_tree)
+                        logging.info("New formula tree added to map_index_to_formula_trees")
 
         elif new_measurement["type"] == "measurement":
+            logging.info("Received measurement instrument")
             # extract relevant values
             measurement = new_measurement["measurement"]
             map_index = new_measurement["map_index"]
             atom_index = new_measurement["atom_index"]
             subatom_index = new_measurement["subatom_index"]
-            logging.info(f"Received measurment instrument with measurement = {measurement}, map_index = {map_index}, atom_index = {atom_index}, subatom_index = {subatom_index}")
+            logging.info("  measurement = %s" % measurement)
+            logging.info("  map_index =  %s" % map_index)
+            logging.info("  atom_index = %s" % atom_index)
+            logging.info("  subatom_index = %s" % subatom_index)
             # get the list of formula trees in map_index_to_formula_trees under the key map_index
             # and attempt to update each one with the measurement
             # Note: a formula tree can only be updated with respect to a measurement once - if the update is attempted
@@ -108,6 +133,7 @@ def monitoring_process_function(online_monitor_object, specification_file):
                 updated_formula_tree = formula_tree.update_with_measurement(measurement, atom_index, subatom_index)
     
     # register verdicts generated by complete bindings
+    logging.info("Constructing dictionary final_map_index_to_formulas_map to contain formula trees for complete bindings")
     final_map_index_to_formulas_map = {}
     for map_index in map_index_to_formula_trees:
         final_map_index_to_formulas_map[map_index] = []
@@ -116,9 +142,11 @@ def monitoring_process_function(online_monitor_object, specification_file):
             # many elements as there are variables in the specification (since each timestamp corresponds
             # to an observation for a single variable)
             if len(formula_tree.get_timestamps()) == len(variables):
+                logging.info("%s is associated with a complete binding" % formula_tree)
                 final_map_index_to_formulas_map[map_index].append(formula_tree)
     
     # register the dictionary of verdicts
+    logging.info("Registering complete verdicts from final_map_index_to_formulas_map")
     online_monitor_object.verdict_queue.put(final_map_index_to_formulas_map)
 
     logging.info("Ending VyPR monitoring process.")
@@ -140,14 +168,19 @@ class OnlineMonitor():
         during a run of the monitored program.
         """
         # set up verdict dictionary ready for the monitoring algorithm to send verdicts
+        logging.info("Initialising empty dictionary to store final formula trees")
         self._map_index_to_formula_trees = {}
         # set up queue for subprocess to read from
+        logging.info("Initialising buffer queue for communication between monitored program")
         self.queue = Queue()
         # set up queue for subprocess to write verdicts to
+        logging.info("Initialising verdict queue for final verdicts")
         self.verdict_queue = Queue()
         # set up the separate process
+        logging.info("Instantiating process for monitoring")
         self.monitoring_process = Process(target=monitoring_process_function, args=(self, specification_file))
         # start the process
+        logging.info("Starting monitoring subprocess")
         self.monitoring_process.start()
     
     def get_new_measurement(self):
