5d00f6ae1aa5110419436a70cdb2ce2c629f3eef
Joshua Heneage Dawes <joshua.dawes@uni.lu>
2021-05-05 12:02:02 +0200

    Add tests for SCFG builder.

---
+++ tests/SCFG/test_builder.py
@@ -0,0 +1,54 @@
+"""
+Module containing tests for the VyPR.SCFG.search module.
+"""
+
+import unittest
+import ast
+import sys
+sys.path.append("..")
+
+import VyPR.Logging.logger as logger
+
+from VyPR.SCFG.builder import SCFG
+from VyPR.SCFG.search import SCFGSearcher
+from VyPR.Specifications.predicates import calls, changes
+from VyPR.Specifications.constraints import ConcreteStateVariable
+
+class TestSCFGBuilder(unittest.TestCase):
+
+    def setUp(self):
+        # initialise logger
+        logger.initialise_logging(directory="../logs/test-logs/instrumentation/")
+        # read code and parse asts
+        with open("test-data/programs/no-procedures-test-1.py") as h:
+            self.code = h.read()
+            self.asts = ast.parse(self.code).body
+        # build scfg
+        self.scfg = SCFG(self.asts)
+        # get root symbolic state
+        self.root_symbolic_state = self.scfg.get_root_symbolic_state()
+    
+    def tearDown(self):
+        # close logging
+        logger.end_logging()
+    
+    def test_get_symbolic_states_from_symbol(self):
+        # get symbolic states
+        symbolic_states = self.scfg.get_symbolic_states_from_symbol('f1')
+        # assertions
+        for symbolic_state in symbolic_states:
+            self.assertListEqual(symbolic_state.get_symbols_changed(), ['f1'])
+    
+    def test_get_reachable_symbolic_states_from_symbol(self):
+        # get symbolic states
+        symbolic_states = self.scfg.get_reachable_symbolic_states_from_symbol('f1', self.root_symbolic_state)
+        # assertions
+        for symbolic_state in symbolic_states:
+            self.assertListEqual(symbolic_state.get_symbols_changed(), ['f1'])
+    
+    def test_get_next_symbolic_states(self):
+        # get next symbolic states
+        symbolic_states = self.scfg.get_next_symbolic_states('f1', self.root_symbolic_state)
+        # assertions
+        for symbolic_state in symbolic_states:
+            self.assertListEqual(symbolic_state.get_symbols_changed(), ['f1'])
